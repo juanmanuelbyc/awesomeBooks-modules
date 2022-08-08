66fd1844a175629845bb3dc24a3488cfc41185eb
juanmanuelbyc <jfdiaza@unal.edu.co>
2022-08-08 19:04:50 -0400

    crate 3 modules for js functionality

---
+++ modules/addEventListeners.js
@@ -0,0 +1,39 @@
+import {submit,container,bookList,addContainer,contactContainer,list,addNew,contact} from './htmlNodes.js';
+import {book} from '../index.js';
+
+function addListeners () {
+
+ document.addEventListener('DOMContentLoaded', () => {
+  book.displayBooks();
+});
+
+submit.addEventListener('click', book.addBook);
+
+container.addEventListener('click', (e) => {
+  if (e.target.classList.contains('remove')) {
+    const targetId = +e.target.getAttribute('id');
+    book.removeBook(targetId);
+  }
+});
+
+list.addEventListener('click', () => {
+  bookList.classList.remove('hidden');
+  addContainer.classList.add('hidden');
+  contactContainer.classList.add('hidden');
+});
+
+addNew.addEventListener('click', () => {
+  bookList.classList.add('hidden');
+  addContainer.classList.remove('hidden');
+  contactContainer.classList.add('hidden');
+});
+
+contact.addEventListener('click', () => {
+  bookList.classList.add('hidden');
+  addContainer.classList.add('hidden');
+  contactContainer.classList.remove('hidden');
+});
+
+}
+
+export {addListeners};
---
+++ modules/htmlNodes.js
@@ -0,0 +1,13 @@
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const submit = document.querySelector('#submit');
+const container = document.querySelector('.data-container');
+const bookList = document.querySelector('.book-list');
+const addContainer = document.querySelector('.add-new');
+const contactContainer = document.querySelector('.contacts');
+const list = document.querySelector('.list');
+const addNew = document.querySelector('.add');
+const contact = document.querySelector('.contact');
+const dateTime = document.querySelector('.date');
+
+export {title, author,submit,container,bookList,addContainer,contactContainer,list,addNew,contact,dateTime};
